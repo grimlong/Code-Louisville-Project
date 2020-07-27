778a04d2083601797f8358426d1b8578dd3a8412
grimlong <jasonmillers197@gmail.com>
2020-07-27 19:46:13 -0400

    Added a lot of comments

--- rollstats.js
+++ rollstats.js
@@ -1,5 +1,8 @@
 // When called by the button in the HTML this function makes 2 random numbers between 1 and 6 and adds 6, (emulating
-// rolling dice for character generation in Dungeons & Dragons) then displays the result in the HTML.
+// rolling 2 six sided dice and adding 6 for character generation in Dungeons & Dragons)
+// then displays the result in the HTML.
+
+
 
 function rollAttribute(att) {
     var Result = document.getElementById(att);
